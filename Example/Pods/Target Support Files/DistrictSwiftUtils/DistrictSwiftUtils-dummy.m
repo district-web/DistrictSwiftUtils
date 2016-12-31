4086734a6bad1d362e703b69505ebe839a1027ce
Maxime <maxime@MacBook-Pro-de-maxime.local>
2016-12-31 12:52:47 +0100

    Update Project to latest syntax

---
+++ Example/Pods/Target Support Files/DistrictSwiftUtils/DistrictSwiftUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DistrictSwiftUtils : NSObject
+@end
+@implementation PodsDummy_DistrictSwiftUtils
+@end
---
+++ Example/Pods/Target Support Files/DistrictSwiftUtils/DistrictSwiftUtils-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double DistrictSwiftUtilsVersionNumber;
+FOUNDATION_EXPORT const unsigned char DistrictSwiftUtilsVersionString[];
+
